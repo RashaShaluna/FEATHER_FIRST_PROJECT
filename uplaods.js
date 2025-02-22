d86ed255d734cf5bbfa926317aacaf18b27d50e2
RashaShaluna <rashashaluna1245@gmail.com>
2025-02-22 23:37:38 +0530

    added og path

--- uplaods.js
+++ uplaods.js
@@ -1,28 +1,25 @@
 const path = require('path');
 const multer = require('multer');
-const uploadPath = path.join(__dirname, "../public/uploads");
 const fs = require('fs')
+const uploadPath = "/home/ubuntu/FEATHER_FIRST_PROJECT/public/uploads/";
 
 if (!fs.existsSync(uploadPath)) {
-  console.log("Uploads folder does NOT exist. Creating it now...");
   fs.mkdirSync(uploadPath, { recursive: true });
-} else {
-  console.log("Uploads folder exists.");
 }
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      console.log("Saving image to:", uploadPath);    
-      cb(null, uploadPath);      
+      cb(null,uploadPath)
+      log('saving in the'uploadPath)
     },
     filename: function (req, file, cb) {
       const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
       cb(null, file.fieldname + '-' + uniqueSuffix +path.extname(file.originalname))
     }
-  })  
+  })
   
 
-  const uploads = multer({storage:storage,limits: { fileSize: 10 * 1024 * 1024 },}) 
+  const uploads = multer({storage:storage})
 
 
 module.exports = uploads;
