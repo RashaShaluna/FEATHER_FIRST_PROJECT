7b29eced4e9b08c9a37dbfc69a053d30b85088c3
RashaShaluna <rashashaluna1245@gmail.com>
2025-02-22 23:24:30 +0530

    added a code in a multer

--- uplaods.js
+++ uplaods.js
@@ -1,17 +1,26 @@
 const path = require('path');
 const multer = require('multer');
+const uploadPath = path.join(__dirname, "../public/uploads");
+const fs = require('fs')
+
+if (!fs.existsSync(uploadPath)) {
+  console.log("Uploads folder does NOT exist. Creating it now...");
+  fs.mkdirSync(uploadPath, { recursive: true });
+} else {
+  console.log("Uploads folder exists.");
+}
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, path.join(__dirname, "../public/uploads"));
-        },
+      console.log("Saving image to:", uploadPath);    
+      cb(null, uploadPath);      
+    },
     filename: function (req, file, cb) {
       const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
       cb(null, file.fieldname + '-' + uniqueSuffix +path.extname(file.originalname))
     }
-  })
+  })  
   
-  console.log('fileee', 'C:/Users/lenovo/OneDrive/Desktop/FIRST_PROJECT_WEEK 8/public/uploads');
 
   const uploads = multer({storage:storage,limits: { fileSize: 10 * 1024 * 1024 },}) 
 
