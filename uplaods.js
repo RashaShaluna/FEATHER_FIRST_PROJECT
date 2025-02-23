84aa3c634f6721b532b50fbb39543e197138c04d
RashaShaluna <rashashaluna1245@gmail.com>
2025-02-23 10:17:55 +0530

    chnaged the codes in multer and addingproduct.ejs

--- uplaods.js
+++ uplaods.js
@@ -1,25 +1,38 @@
 const path = require('path');
 const multer = require('multer');
-const fs = require('fs')
-const uploadPath = "/home/ubuntu/FEATHER_FIRST_PROJECT/public/uploads/";
+const fs = require('fs');
 
+const uploadPath = path.join(__dirname, "../public/uploads");
 if (!fs.existsSync(uploadPath)) {
   fs.mkdirSync(uploadPath, { recursive: true });
 }
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null,uploadPath)
-      log('saving in the',uploadPath)
+      cb(null, uploadPath);
     },
     filename: function (req, file, cb) {
       const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-      cb(null, file.fieldname + '-' + uniqueSuffix +path.extname(file.originalname))
+      cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname))
     }
-  })
-  
+});
 
-  const uploads = multer({storage:storage})
+const uploads = multer({
+    storage: storage,
+    limits: {
+        fileSize: 50 * 1024 * 1024  // 50MB in bytes
+    },
+    fileFilter: function (req, file, cb) {
+        const filetypes = /jpeg|jpg|png|gif/;
+        const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+        const mimetype = filetypes.test(file.mimetype);
 
+        if (extname && mimetype) {
+            return cb(null, true);
+        } else {
+            cb('Error: Images only!');
+        }
+    }
+});
 
 module.exports = uploads;
